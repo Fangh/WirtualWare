414a1f77a99b6358566321727b41f8ac1a42cc28
TheKaijuGirl <40342644+TheKaijuGirl@users.noreply.github.com>
2020-09-12 19:57:54 +0200

    Score main

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,19 +9,26 @@ public class ScoreManager : MonoBehaviour
     public int score;
     public TextMeshPro textScore;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        GameManager.Instance.levelManager.OnLevelLoaded += PositionScoreText;
+    }
+
+    private void PositionScoreText()
+    {
+        GameObject textScoreOBJ = GameObject.FindGameObjectWithTag("ScoreText");
+        textScore = textScoreOBJ.GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     public void UpdateScore()
     {
-
         score++;
         textScore.text = "Score : " + score;
 
