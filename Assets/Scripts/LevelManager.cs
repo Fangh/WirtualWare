a6d56af3af0ae6186c028809bcddde1be9029138
Thomas-6freedom <49686866+Thomas-6freedom@users.noreply.github.com>
2020-09-12 19:21:05 +0200

    no more common scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,14 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private string firstSceneName;
-
-    public void Start()
-    {
-        Debug.Log("Level Manager START");
-        LoadLevel(firstSceneName);
-    }
-
     public void LoadLevel(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
