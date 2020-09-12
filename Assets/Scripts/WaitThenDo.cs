4cb45f67e145dc842849c65f43c27c61c2c9cdbc
Thomas-6freedom <49686866+Thomas-6freedom@users.noreply.github.com>
2020-09-12 19:57:56 +0200

    waitthendo

---
+++ Assets/Scripts/WaitThenDo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitThenDo : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.Events.UnityEvent OnWaitEnd;
+
+    private float currentWaitTime = 0;
+    private bool isStarted = false;
+
+    public void Wait(float _time)
+    {
+        currentWaitTime = _time;
+        isStarted = true;
+    }
+
+
+    private void Update()
+    {
+        if (isStarted)
+        {
+            if (currentWaitTime <= 0)
+            {
+                OnWaitEnd?.Invoke();
+                isStarted = false;
+            }
+            else
+                currentWaitTime -= Time.deltaTime;
+        }
+    }
+}
