ce893fd9a6c8b74b3151f524d9c37273af15312c
TheKaijuGirl <40342644+TheKaijuGirl@users.noreply.github.com>
2020-09-13 12:36:44 +0200

    Final i guess

--- Assets/Scripts/Chamboultou/Ball.cs
+++ Assets/Scripts/Chamboultou/Ball.cs
@@ -8,9 +8,15 @@ public class Ball : MonoBehaviour
     public Transform respawnPoint;
     Rigidbody rb;
 
+    public GameObject canPrefab;
+    public GameObject canToDestroy;
+    public GameObject canSpawnPoint;
+    public bool collisionBall;
+
     // Start is called before the first frame update
     void Start()
     {
+        collisionBall = false;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -26,7 +32,26 @@ public class Ball : MonoBehaviour
         if (other.gameObject.tag == "Floor")
         {
             gameObject.transform.position = respawnPoint.position;
-            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
+            rb.isKinematic = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Can" && collisionBall == false)
+        {
+            collisionBall = true;
+            Invoke("Relaunch", 2f);
+        }
+    }
+
+
+    void Relaunch()
+    {
+        Destroy(canToDestroy);
+        GameObject newcan = Instantiate(canPrefab, canSpawnPoint.transform.position, Quaternion.Euler(0,90,0));
+        canToDestroy = newcan;
+        collisionBall = false;
     }
 }
